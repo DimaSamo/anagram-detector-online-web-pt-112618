1e7ff081d0626aedd9b173899a0d9b77020c7882
Dmitri Samotessov <dimasamo96@gmail.com>
2018-12-18 09:01:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,21 @@
 # Your code goes here!
 class Anagram
   attr_accessor :word
+
   def initialize(word)
     @word = word
+  end
+
+  def match(words)
+    words.collect do |element|
+      truth = false
+      if self.word.length == element.length
+        truth = true
+        element.split(//).each do |char|
+          truth = false unless self.word.include?(char)
+        end
+      end
+    end
+    element if truth
   end
 end

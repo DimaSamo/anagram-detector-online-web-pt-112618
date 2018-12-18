5e48d1ff8a5e99733f5f31d41e6c7615e59fa829
Dmitri Samotessov <dimasamo96@gmail.com>
2018-12-18 09:04:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -14,6 +14,7 @@ class Anagram
         truth = true
         element.split(//).each do |char|
           truth = false unless self.word.include?(char)
+          binding.pry
         end
       end
           element if truth
